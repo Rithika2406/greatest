b1b600ba5b2451e7d1b12bb945992d5cab100a32
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-11-18 18:24:55 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 5, y = 5;
+    if (x > y)
+      System.out.println("x is greater");
+    else if (y >= x)
+      System.out.println("y is greater");
+    else
+      System.out.println("Both are equal");
+  }
+}
